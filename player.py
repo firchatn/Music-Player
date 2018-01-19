179b46217becf8bc780c0545978cf68b7904df1e
Firas Chaaben <firaschaabencss@gmail.com>
2018-01-19 22:09:39 +0100

    tree view list

--- player.py
+++ player.py
@@ -3,13 +3,30 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 import random
+import os
 
 # init variables
 
 ispaused = False
 
+
+
+
+
+def treeviewItems():
+    store = Gtk.ListStore(str)
+    files = [f for f in os.listdir('.') if os.path.isfile(f)]
+    for f in files:
+        store.append ([f])
+    #for key, value in docs.items():
+    #    store.append ([value])
+    treeview.set_model(store)
+    rendererText = Gtk.CellRendererText()
+    column = Gtk.TreeViewColumn("Defined User List", rendererText, text=0)
+    treeview.append_column(column)
+
+
 class Handler:
-    
     def onDeleteWindow(self, *args):
         Gtk.main_quit(*args)
 
@@ -43,6 +60,13 @@ window.set_default_size(700, 300)
 image = builder.get_object("image1")
 image.set_from_file('player.png')
 
+progress = builder.get_object("progressbar1")
+progress.set_fraction(1)
+
+progress.set_text("player on")
+progress.pulse()
+treeview = builder.get_object("treeview1")
+treeviewItems()
 window.show_all()
 
 
