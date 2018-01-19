4933a68b5a15e198e3c85d0c0d63b55c116007d2
Firas Chaaben <firaschaabencss@gmail.com>
2018-01-19 21:23:49 +0100

    first success test

--- player.py
+++ player.py
@@ -1,7 +1,30 @@
-from pygame import mixer 
+from pygame import mixer
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+import random
+
+class Handler:
+    def onDeleteWindow(self, *args):
+        Gtk.main_quit(*args)
+
+    def playmusic(self,play):
+        mixer.music.play()
+
+            
+    
+builder = Gtk.Builder()
+builder.add_from_file("player.glade")
+builder.connect_signals(Handler())
 
 mixer.init()
 mixer.music.load('re.mp3')
 
-def playmusic():
-    mixer.music.play()
+window = builder.get_object("window1")
+window.connect("delete-event", Gtk.main_quit)
+window.set_default_size(600, 500)
+window.show_all()
+
+
+Gtk.main()
+
