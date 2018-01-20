4900365537a68c7b20b67ab7f5b6d8b2b3d61871
Firas Chaaben <firaschaabencss@gmail.com>
2018-01-20 22:05:24 +0100

    support playlist

--- player.py
+++ player.py
@@ -4,12 +4,34 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 import random
 import os
+import json
 
 # init variables
 
 ispaused = False
+pathdata = 'data/playlist.json'
+store = Gtk.ListStore(str)
 
 #     methods
+
+def opend():
+    if doesFileExists(path):
+        json_data=open(path)
+        statistics = json.load(json_data)
+    else:
+        statistics = {"default": 1}
+        with open(path, 'w') as outfile:
+            json.dump(statistics, outfile)
+    return statistics
+
+def openplaylist():
+    json_data=open(pathdata)
+    docs = json.load(json_data)
+    return docs
+
+def updateplaylist(choice):
+    store.append([choice])
+    
 
 def choicefile(openfile):
     filter = Gtk.FileFilter()
@@ -28,19 +50,29 @@ def choicefile(openfile):
     elif response == Gtk.ResponseType.CANCEL:
         return False
     choice = dialog.get_filename()
+    docs = openplaylist()
+    print(len(docs))
+    docs[str(len(docs)+1)] = choice
+    with open(pathdata, 'w') as outfile:
+            json.dump(docs, outfile)
+    updateplaylist(choice)
     dialog.destroy()
     progress.set_text(choice)
     mixer.music.load(choice)
     return choice
 
 def treeviewItems():
-    store = Gtk.ListStore(str)
+    
     files = [f for f in os.listdir('.') if os.path.isfile(f)]
-    for f in files:
-        store.append ([f])
+    #for f in files:
+    #    store.append ([f])
+    playlistitems = openplaylist()
+    for key, value in playlistitems.items():
+        print(key , value)
+        store.append([value])
     treeview.set_model(store)
     rendererText = Gtk.CellRendererText()
-    column = Gtk.TreeViewColumn("Defined User List", rendererText, text=0)
+    column = Gtk.TreeViewColumn("PlayList", rendererText, text=0)
     treeview.append_column(column)
 
 def progressplus():
@@ -90,7 +122,7 @@ builder.add_from_file("player.glade")
 builder.connect_signals(Handler())
 
 mixer.init()
-mixer.music.load('re.mp3')
+mixer.music.load('data/re.mp3')
 
 window = builder.get_object("window1")
 window.connect("delete-event", Gtk.main_quit)
